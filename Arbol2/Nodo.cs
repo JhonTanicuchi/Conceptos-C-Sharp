41563891a04eb62407688beb691c18aaad3258ad
JhonTanicuchi <jav.tanicuchi.@yavirac.edu.ec>
2021-06-24 14:16:20 -0500

    Programa C# consola, Arboles 'Operaciones con Nodos'

---
+++ Arbol2/Nodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arbol2
+{
+    class Nodo
+    {
+        private string _valor;
+
+
+        public string Valor
+        {
+            get { return _valor; }
+            set { _valor = value; }
+        }
+        public Nodo Izquierdo { get; set; }
+        public Nodo Derecho { get; set; }
+        public List<Nodo> Nodos { get; internal set; } = new List<Nodo>();
+
+
+        public Nodo() {}
+
+
+        public Nodo(string valor)
+        {
+            Valor = valor;
+        }      
+    }
+}
---
+++ Arbol2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arbol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            var raiz = new Nodo
+            {
+                Valor = "*",
+                Izquierdo = new Nodo()
+                {
+                    Valor = "+",
+                    Izquierdo = new Nodo("5"),
+                    Derecho = new Nodo("3")
+                },
+
+                Derecho = new Nodo()
+                {
+                    Valor = "+",
+                    Izquierdo = new Nodo("7"),                 
+                    Derecho = new Nodo("5")                    
+                }
+            };
+
+            var suma1 = int.Parse(raiz.Izquierdo.Izquierdo.Valor) + int.Parse(raiz.Izquierdo.Derecho.Valor);
+            var suma2 = int.Parse(raiz.Derecho.Izquierdo.Valor) + int.Parse(raiz.Derecho.Derecho.Valor);
+            var multiplicacion = suma1 * suma2;
+            Console.WriteLine(multiplicacion);
+        }
+    }
+}
