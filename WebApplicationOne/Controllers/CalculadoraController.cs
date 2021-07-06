48d32a055c602a004aa2e9001b84f4e24213d934
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2021-07-05 22:05:30 -0500

    Aplicación Web C#

--- WebApplicationOne/Controllers/CalculadoraController.cs
+++ WebApplicationOne/Controllers/CalculadoraController.cs
@@ -14,15 +14,36 @@ namespace WebApplicationOne.Controllers
         }
 
         [HttpPost]
-        public IActionResult Calcular(int numero_1, int numero_2)
+        public IActionResult Calcular(int numero_1, int numero_2, string operacion)
         {
-            //TODO: Como acceder a Request.Form desde asp.net core
-            var numero_1FromBody = int.Parse(Request.Form["numero_1"]); 
+            var resultadoFromBody = 0;
+            var resultado = 0;
+
+            var numero_1FromBody = int.Parse(Request.Form["numero_1"]);
             var numero_2FromBody = int.Parse(Request.Form["numero_2"]);
 
-            var resultadoFromBody = numero_1FromBody + numero_2FromBody;
-            var resultado = numero_1 + numero_2;
-
+            switch (operacion)
+            {
+                case "+":
+                    resultadoFromBody = numero_1FromBody + numero_2FromBody;
+                    resultado = numero_1 + numero_2;
+                    break;
+                case "-":
+                    resultadoFromBody = numero_1FromBody - numero_2FromBody;
+                    resultado = numero_1 - numero_2;
+                    break;
+                case "*":
+                    resultadoFromBody = numero_1FromBody * numero_2FromBody;
+                    resultado = numero_1 * numero_2;
+                    break;
+                case "/":
+                    resultadoFromBody = numero_1FromBody / numero_2FromBody;
+                    resultado = numero_1 / numero_2;
+                    break;
+                default:                  
+                    break;
+            }
+          
             ViewData["ResultadoFromBody"] = resultadoFromBody;
             ViewData["Resultado"] = resultado;
             return View();
