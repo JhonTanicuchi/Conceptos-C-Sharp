04606d4580656844729f46a75d214b1b65edc225
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2021-07-05 21:03:37 -0500

    Aplicación Web C#

---
+++ WebApplicationOne/Controllers/CalculadoraController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationOne.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(int numero_1, int numero_2)
+        {
+            //TODO: Como acceder a Request.Form desde asp.net core
+            var numero_1FromBody = int.Parse(Request.Form["numero_1"]); 
+            var numero_2FromBody = int.Parse(Request.Form["numero_2"]);
+
+            var resultadoFromBody = numero_1FromBody + numero_2FromBody;
+            var resultado = numero_1 + numero_2;
+
+            ViewData["ResultadoFromBody"] = resultadoFromBody;
+            ViewData["Resultado"] = resultado;
+            return View();
+        }
+    }
+}
